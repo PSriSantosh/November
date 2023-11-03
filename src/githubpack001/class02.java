fd06c10a0eb81ad961681962748e365eb14f4dd3
admin <admin@DESKTOP-0AO7GME>
2023-11-03 16:58:58 +0530

    hii

---
+++ src/githubpack001/class02.java
@@ -0,0 +1,13 @@
+package githubpack001;
+
+public class class02 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Maheshwari details");
+		System.out.println("Emp-Id: MT-01426");
+		System.out.println("Tester");
+		
+	}
+
+}
